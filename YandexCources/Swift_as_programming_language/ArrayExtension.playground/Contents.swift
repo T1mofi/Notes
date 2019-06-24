31a3713f4ec3ca192519ccda0fa43bcf3e1e94a1
Tsimofei Sikorski <sikorskitimofei@gmail.com>
2019-06-24 10:59:08 +0300

    end Erray Extension task

---
+++ YandexCources/Swift_as_programming_language/ArrayExtension.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension Array {
+    mutating func myShuffle() {
+         self.shuffle()
+    }
+    
+    func myShuffled() -> Array {
+        return self.shuffled()
+    }
+}
+
+
