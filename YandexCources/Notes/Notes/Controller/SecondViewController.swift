81f6ef5fd1a7a14c3015699b5945f6f7fce02a3a
Tsimofei Sikorski <sikorskitimofei@gmail.com>
2019-08-12 17:54:48 +0300

    add Segue

---
+++ YandexCources/Notes/Notes/Controller/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  Notes
+//
+//  Created by Timofei Sikorski on 8/12/19.
+//  Copyright © 2019 SikorskiIT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    var text: String = "prepare segue err"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mainLabel.text = text
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- YandexCources/Notes/Notes/Controller/ViewController.swift
+++ YandexCources/Notes/Notes/Controller/ViewController.swift
@@ -10,45 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBAction func showScreenButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "ShowSecondScreen", sender: nil)
+    }
+    
+    @IBAction func unwindToSrartScreen(segue: UIStoryboardSegue) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let controller = segue.destination as? SecondViewController,
+        segue.identifier == "ShowSecondScreen" {
+            controller.text = "Super Mega Text"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let note:Note = Note(
-            title: "MyTitle",
-            content: "MyContent",
-            color: UIColor.yellow,
-            importance: .important,
-            selfDestructionDate: Calendar.current.date(byAdding: .day, value: 7, to: Date()))
-        
-        let note2:Note = Note(
-            title: "MyTitle",
-            content: "MyContent",
-            color: UIColor.yellow,
-            importance: .important,
-            selfDestructionDate: Calendar.current.date(byAdding: .day, value: 7, to: Date()))
-        
-    
-        print(note.json)
-        print("Note json\n")
-        
-        let noteFromJSON = Note.parse(json: note.json)
-        print(noteFromJSON?.json ?? "nil")
-        print("noteFromJSON json\n")
-        
-    
-        let notebook = FileNotebook()
-        
-        notebook.add(note)
-        notebook.add(note2)
-        
-        notebook.saveToFile()
-        
-        for key in notebook.notes.keys {
-            notebook.remove(with: key)
-        }
-        
-        notebook.loadFromFile()
+//        let note:Note = Note(
+//            title: "MyTitle",
+//            content: "MyContent",
+//            color: UIColor.yellow,
+//            importance: .important,
+//            selfDestructionDate: Calendar.current.date(byAdding: .day, value: 7, to: Date()))
+//
+//        let note2:Note = Note(
+//            title: "MyTitle",
+//            content: "MyContent",
+//            color: UIColor.yellow,
+//            importance: .important,
+//            selfDestructionDate: Calendar.current.date(byAdding: .day, value: 7, to: Date()))
+//
+//
+//        print(note.json)
+//        print("Note json\n")
+//
+//        let noteFromJSON = Note.parse(json: note.json)
+//        print(noteFromJSON?.json ?? "nil")
+//        print("noteFromJSON json\n")
+//
+//
+//        let notebook = FileNotebook()
+//
+//        notebook.add(note)
+//        notebook.add(note2)
+//
+//        notebook.saveToFile()
+//
+//        for key in notebook.notes.keys {
+//            notebook.remove(with: key)
+//        }
+//
+//        notebook.loadFromFile()
 
     }
 }
