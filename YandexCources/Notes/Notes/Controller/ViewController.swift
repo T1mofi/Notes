45776566d8a0b97a02280d11b8e1388af039c5f2
Tsimofei Sikorski <sikorskitimofei@gmail.com>
2019-08-27 16:09:08 +0300

    make UI sketch

--- YandexCources/Notes/Notes/Controller/ViewController.swift
+++ YandexCources/Notes/Notes/Controller/ViewController.swift
@@ -9,46 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func switchToggled(_ sender: Any) {
+        datePicker.isHidden.toggle()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
         
-        let note:Note = Note(
-            title: "MyTitle",
-            content: "MyContent",
-            color: UIColor.yellow,
-            importance: .important,
-            selfDestructionDate: Calendar.current.date(byAdding: .day, value: 7, to: Date()))
-        
-        let note2:Note = Note(
-            title: "MyTitle",
-            content: "MyContent",
-            color: UIColor.yellow,
-            importance: .important,
-            selfDestructionDate: Calendar.current.date(byAdding: .day, value: 7, to: Date()))
-        
-    
-        print(note.json)
-        print("Note json\n")
-        
-        let noteFromJSON = Note.parse(json: note.json)
-        print(noteFromJSON?.json ?? "nil")
-        print("noteFromJSON json\n")
-        
-    
-        let notebook = FileNotebook()
-        
-        notebook.add(note)
-        notebook.add(note2)
-        
-        notebook.saveToFile()
-        
-        for key in notebook.notes.keys {
-            notebook.remove(with: key)
-        }
-        
-        notebook.loadFromFile()
+//        let note:Note = Note(
+//            title: "MyTitle",
+//            content: "MyContent",
+//            color: UIColor.yellow,
+//            importance: .important,
+//            selfDestructionDate: Calendar.current.date(byAdding: .day, value: 7, to: Date()))
+//
+//        let note2:Note = Note(
+//            title: "MyTitle",
+//            content: "MyContent",
+//            color: UIColor.yellow,
+//            importance: .important,
+//            selfDestructionDate: Calendar.current.date(byAdding: .day, value: 7, to: Date()))
+//
+//
+//        print(note.json)
+//        print("Note json\n")
+//
+//        let noteFromJSON = Note.parse(json: note.json)
+//        print(noteFromJSON?.json ?? "nil")
+//        print("noteFromJSON json\n")
+//
+//
+//        let notebook = FileNotebook()
+//
+//        notebook.add(note)
+//        notebook.add(note2)
+//
+//        notebook.saveToFile()
+//
+//        for key in notebook.notes.keys {
+//            notebook.remove(with: key)
+//        }
+//
+//        notebook.loadFromFile()
 
     }
 }
