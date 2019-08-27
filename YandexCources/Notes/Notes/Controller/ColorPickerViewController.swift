17edbe2ef75f2bb8e114ef7ed53700d7d6d0c54d
Tsimofei Sikorski <sikorskitimofei@gmail.com>
2019-08-27 21:06:01 +0300

    Add ColorPicker

---
+++ YandexCources/Notes/Notes/Controller/ColorPickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorPickerViewController.swift
+//  Notes
+//
+//  Created by Timofei Sikorski on 8/27/19.
+//  Copyright © 2019 SikorskiIT. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
